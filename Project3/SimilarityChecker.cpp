461150eb4a4f17757a8687f0af090cff1bc8ae38
jiyeon <jiyeon08.you@samsung.com>
2024-07-03 17:05:54 +0900

    [refactoring] 1

--- Project3/SimilarityChecker.cpp
+++ Project3/SimilarityChecker.cpp
@@ -6,10 +6,11 @@ using namespace std;
 class SimilarityChecker {
 public:
 	int lengthCheck(string str1, string str2) {
-		if ((str1.length() > str2.length()) && 
-			(str1.length() > str2.length() * 2)) {
+		if ((str1.length() > str2.length() && 
+			str1.length() > str2.length() * 2) ||
+			(str2.length() > str1.length() &&
+			str2.length() > str1.length() * 2)) {
 			return 0;
 		}
 	}
-
 };
