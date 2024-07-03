a25f45a714f4e652ac5215a6ac14d9db15e03426
jiyeon <jiyeon08.you@samsung.com>
2024-07-04 08:53:02 +0900

    [refactoring] 4

--- Project3/SimilarityChecker.cpp
+++ Project3/SimilarityChecker.cpp
@@ -5,29 +5,24 @@ using namespace std;
 
 class SimilarityChecker {
 public:
-	int lengthCheck(string str1, string str2) {
+	double lengthCheck(string str1, string str2) {
 		int length1 = str1.length();
 		int length2 = str2.length();
 
-		if (length1 > length2 * 2 ||
-			length2 > length1 * 2) {
+		if (length1 > length2 * 2 || length2 > length1 * 2) {
 			return 0;
 		}
 		if (length1 == length2) {
 			return LENGTH_MAX_SCORE;
 		}
-		double gap, b;
-		if (length1 > length2) {
-			gap = length1 - length2;
-			b = length2;
-		}
-		else {
-			gap = length2 - length1;
-			b = length1;
-		}
-		return (1 - (gap / b)) * 60;
-		
+
+		double maxLength = static_cast<double>(max(length1, length2));
+		double minLength = static_cast<double>(min(length1, length2));
+		double gap = maxLength - minLength;
+
+		return (1.0 - (gap / minLength)) * LENGTH_MAX_SCORE; 
 	}
+
 private:
-	const int LENGTH_MAX_SCORE = 60;
+	const double LENGTH_MAX_SCORE = 60;
 };
